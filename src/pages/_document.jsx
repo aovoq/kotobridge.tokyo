ba963d2e997ca58b2bb523fbb1a17983d9f3e8c7
aovoq <a21dc163@dhw.ac.jp>
2022-06-08 15:39:25 +0900

    feat: google font optimized

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,5 +1,3 @@
-// import Document, { Html, Head, Main, NextScript } from 'next/document'
-
 // class MyDocument extends Document {
 //    static async getInitialProps(ctx) {
 //       const initialProps = await Document.getInitialProps(ctx)
@@ -21,29 +19,43 @@
 
 // export default MyDocument
 
-
-import Document from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+   static async getInitialProps(ctx) {
+      const sheet = new ServerStyleSheet()
+      const originalRenderPage = ctx.renderPage
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        })
+      try {
+         ctx.renderPage = () =>
+            originalRenderPage({
+               enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+            })
 
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: [initialProps.styles, sheet.getStyleElement()],
+         const initialProps = await Document.getInitialProps(ctx)
+         return {
+            ...initialProps,
+            styles: [initialProps.styles, sheet.getStyleElement()],
+         }
+      } finally {
+         sheet.seal()
       }
-    } finally {
-      sheet.seal()
-    }
-  }
+   }
+
+   render() {
+      return (
+         <Html lang='ja'>
+            <Head>
+               {/* <link rel='preconnect' href='https://fonts.googleapis.com' />
+               <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin /> */}
+               <link href='https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap' rel='stylesheet' />
+            </Head>
+            <body>
+               <Main />
+               <NextScript />
+            </body>
+         </Html>
+      )
+   }
 }

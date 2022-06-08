6dc23e6d931e27c35c6297c107ec0a98c75bb2c5
aovoq <a21dc163@dhw.ac.jp>
2022-06-08 18:38:44 +0900

    fix: I thought roboto was the built-in font?

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -27,6 +27,7 @@ export default class MyDocument extends Document {
          <Html lang='ja'>
             <Head>
                <link href='https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap' rel='stylesheet' />
+               <link href='https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;900&display=swap' rel='stylesheet' />
             </Head>
             <body>
                <Main />

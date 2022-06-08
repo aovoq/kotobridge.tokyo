1ad980a0cf0dc1714fe5676fdc395a2cf409d3ff
aovoq <a21dc163@dhw.ac.jp>
2022-06-08 16:04:24 +0900

    fix: font optimization was not experimental

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,24 +1,3 @@
-// class MyDocument extends Document {
-//    static async getInitialProps(ctx) {
-//       const initialProps = await Document.getInitialProps(ctx)
-//       return { ...initialProps }
-//    }
-
-//    render() {
-//       return (
-//          <Html lang="ja">
-//             <Head />
-//             <body>
-//                <Main />
-//                <NextScript />
-//             </body>
-//          </Html>
-//       )
-//    }
-// }
-
-// export default MyDocument
-
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
@@ -47,8 +26,6 @@ export default class MyDocument extends Document {
       return (
          <Html lang='ja'>
             <Head>
-               {/* <link rel='preconnect' href='https://fonts.googleapis.com' />
-               <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin /> */}
                <link href='https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap' rel='stylesheet' />
             </Head>
             <body>

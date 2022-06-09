8da49e821409c7b8bd0500b29300b55111a0ecae
aovoq <a21dc163@dhw.ac.jp>
2022-06-10 04:38:24 +0900

    fix: firstview wip

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import Layout from '../components/layout'
 import styled from 'styled-components'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef } from 'react'
 import normalizeWheel from 'normalize-wheel'
 import { createGlobalStyle } from 'styled-components'
 import Link from 'next/link'
@@ -38,11 +38,8 @@ const BridgeImage = styled.div`
       margin-left: 40vh;
    }
 `
-const Text = styled.div`
-   display: inline-block;
-`
 
-const Item = styled.div`
+const Itemx = styled.div`
    position: relative;
    display: inline-block;
    margin-left: 500px;
@@ -63,6 +60,63 @@ const Test = styled.div`
    height: 100%;
    background-size: cover;
    background-position: center;
+`
+
+const Upper = styled.div`
+   position: relative;
+   top: 15%;
+   display: flex;
+   gap: 240px;
+   margin-left: 75px;
+`
+
+const Lower = styled.div`
+   position: relative;
+   top: 25%;
+   display: flex;
+   gap: 240px;
+   margin-left: 458px;
+`
+
+const Item = styled.div``
+const AnkerWrap = styled.a``
+const BridgeImg = styled.div`
+   background: url('images/kiyosu-bridge.jpg');
+   background-size: cover;
+   background-position: center;
+   width: 500px;
+   height: 281.25px;
+`
+const OuterText = styled.div`
+   color: #152e71;
+   font-weight: 900;
+   font-size: 36px;
+   line-height: 1;
+   text-align: right;
+   &::before {
+      content: '';
+      display: block;
+      width: 0;
+      height: 0;
+      margin-top: -4.9px;
+   }
+`
+
+const ImgWrap = styled.div`
+   position: relative;
+   line-height: 1.06;
+
+`
+
+const InnerText = styled.div`
+   position: absolute;
+   bottom: 0;
+   right: 0;
+   font-size: 80px;
+   font-family: 'Noto sans JP';
+   font-weight: 800;
+   color: rgba(238, 242, 244, 0.6);
+   -webkit-text-stroke: 1px #152E71;
 `
 
 const Home = () => {
@@ -159,60 +213,142 @@ const Home = () => {
          <GlobalStyles />
          <Container>
             <Screen ref={screenRef}>
-               <Item>
-                  <Link href='/bridge/kiyosu-bridge'>
-                     <a>
-                        <ImgWrapper>
-                           <Test src='images/kiyosu-bridge.jpg' className='bridgeImg' />
-                        </ImgWrapper>
-                     </a>
-                  </Link>
-               </Item>
-               <Item>
-                  <Link href='/bridge/kiyosu-bridge'>
-                     <a>
-                        <ImgWrapper>
-                           <Test src='images/kiyosu-bridge.jpg' className='bridgeImg' />
-                        </ImgWrapper>
-                     </a>
-                  </Link>
-               </Item>
-               <Item>
-                  <Link href='/bridge/kiyosu-bridge'>
-                     <a>
-                        <ImgWrapper>
-                           <Test src='images/kiyosu-bridge.jpg' className='bridgeImg' />
-                        </ImgWrapper>
-                     </a>
-                  </Link>
-               </Item>
-               <Item>
-                  <Link href='/bridge/kiyosu-bridge'>
-                     <a>
-                        <ImgWrapper>
-                           <Test src='images/kiyosu-bridge.jpg' className='bridgeImg' />
-                        </ImgWrapper>
-                     </a>
-                  </Link>
-               </Item>
-               <Item>
-                  <Link href='/bridge/kiyosu-bridge'>
-                     <a>
-                        <ImgWrapper>
-                           <Test src='images/kiyosu-bridge.jpg' className='bridgeImg' />
-                        </ImgWrapper>
-                     </a>
-                  </Link>
-               </Item>
-               <Item>
-                  <Link href='/bridge/kiyosu-bridge'>
-                     <a>
-                        <ImgWrapper>
-                           <Test src='images/kiyosu-bridge.jpg' className='bridgeImg' />
-                        </ImgWrapper>
-                     </a>
-                  </Link>
-               </Item>
+               <Upper>
+                  <Item>
+                     <Link href='bridge/kiyosu-bridge'>
+                        <AnkerWrap>
+                           <ImgWrap>
+                              <BridgeImg />
+                              <InnerText>清洲橋</InnerText>
+                           </ImgWrap>
+                           <OuterText>KIYOSU-BRIDGE</OuterText>
+                        </AnkerWrap>
+                     </Link>
+                  </Item>
+                  <Item>
+                     <Link href='bridge/kiyosu-bridge'>
+                        <AnkerWrap>
+                           <ImgWrap>
+                              <BridgeImg />
+                              <InnerText>清洲橋</InnerText>
+                           </ImgWrap>
+                           <OuterText>KIYOSU-BRIDGE</OuterText>
+                        </AnkerWrap>
+                     </Link>
+                  </Item>
+                  <Item>
+                     <Link href='bridge/kiyosu-bridge'>
+                        <AnkerWrap>
+                           <ImgWrap>
+                              <BridgeImg />
+                              <InnerText>清洲橋</InnerText>
+                           </ImgWrap>
+                           <OuterText>KIYOSU-BRIDGE</OuterText>
+                        </AnkerWrap>
+                     </Link>
+                  </Item>
+                  <Item>
+                     <Link href='bridge/kiyosu-bridge'>
+                        <AnkerWrap>
+                           <ImgWrap>
+                              <BridgeImg />
+                              <InnerText>清洲橋</InnerText>
+                           </ImgWrap>
+                           <OuterText>KIYOSU-BRIDGE</OuterText>
+                        </AnkerWrap>
+                     </Link>
+                  </Item>
+                  <Item>
+                     <Link href='bridge/kiyosu-bridge'>
+                        <AnkerWrap>
+                           <ImgWrap>
+                              <BridgeImg />
+                              <InnerText>清洲橋</InnerText>
+                           </ImgWrap>
+                           <OuterText>KIYOSU-BRIDGE</OuterText>
+                        </AnkerWrap>
+                     </Link>
+                  </Item>
+                  <Item>
+                     <Link href='bridge/kiyosu-bridge'>
+                        <AnkerWrap>
+                           <ImgWrap>
+                              <BridgeImg />
+                              <InnerText>清洲橋</InnerText>
+                           </ImgWrap>
+                           <OuterText>KIYOSU-BRIDGE</OuterText>
+                        </AnkerWrap>
+                     </Link>
+                  </Item>
+               </Upper>
+               <Lower>
+                  <Item>
+                     <Link href='bridge/kiyosu-bridge'>
+                        <AnkerWrap>
+                           <ImgWrap>
+                              <BridgeImg />
+                              <InnerText>清洲橋</InnerText>
+                           </ImgWrap>
+                           <OuterText>KIYOSU-BRIDGE</OuterText>
+                        </AnkerWrap>
+                     </Link>
+                  </Item>
+                  <Item>
+                     <Link href='bridge/kiyosu-bridge'>
+                        <AnkerWrap>
+                           <ImgWrap>
+                              <BridgeImg />
+                              <InnerText>清洲橋</InnerText>
+                           </ImgWrap>
+                           <OuterText>KIYOSU-BRIDGE</OuterText>
+                        </AnkerWrap>
+                     </Link>
+                  </Item>
+                  <Item>
+                     <Link href='bridge/kiyosu-bridge'>
+                        <AnkerWrap>
+                           <ImgWrap>
+                              <BridgeImg />
+                              <InnerText>清洲橋</InnerText>
+                           </ImgWrap>
+                           <OuterText>KIYOSU-BRIDGE</OuterText>
+                        </AnkerWrap>
+                     </Link>
+                  </Item>
+                  <Item>
+                     <Link href='bridge/kiyosu-bridge'>
+                        <AnkerWrap>
+                           <ImgWrap>
+                              <BridgeImg />
+                              <InnerText>清洲橋</InnerText>
+                           </ImgWrap>
+                           <OuterText>KIYOSU-BRIDGE</OuterText>
+                        </AnkerWrap>
+                     </Link>
+                  </Item>
+                  <Item>
+                     <Link href='bridge/kiyosu-bridge'>
+                        <AnkerWrap>
+                           <ImgWrap>
+                              <BridgeImg />
+                              <InnerText>清洲橋</InnerText>
+                           </ImgWrap>
+                           <OuterText>KIYOSU-BRIDGE</OuterText>
+                        </AnkerWrap>
+                     </Link>
+                  </Item>
+                  <Item>
+                     <Link href='bridge/kiyosu-bridge'>
+                        <AnkerWrap>
+                           <ImgWrap>
+                              <BridgeImg />
+                              <InnerText>清洲橋</InnerText>
+                           </ImgWrap>
+                           <OuterText>KIYOSU-BRIDGE</OuterText>
+                        </AnkerWrap>
+                     </Link>
+                  </Item>
+               </Lower>
                {/* <Text>1</Text>
                <BridgeImage className='image' />
                <Text>2</Text>

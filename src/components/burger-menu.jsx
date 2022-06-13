fcfa312fa5be1af3325601372ad3e27329ebbb0c
aovoq <a21dc163@dhw.ac.jp>
2022-06-14 06:53:49 +0900

    new: burger-menu (wip

---
+++ src/components/burger-menu.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+   position: fixed;
+   top: 0;
+   left: 0;
+   width: 100%;
+   height: 100%;
+   z-index: 1200;
+   background: rgba(255, 255, 255, 0.8);
+   display: none;
+   transition: 0.3s;
+`
+
+const BurgerMenu = (props) => {
+   return <Container style={{display: props.menuVisibility === true && 'block' }}></Container>
+}
+
+export default BurgerMenu
